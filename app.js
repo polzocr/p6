2ca27fc420576a60aa628b80e350142684649be8
Polzocr <micro.cpt.62@outlook.fr>
2022-03-21 11:22:47 +0100

    ajout de dotenv et des variables d'environnement pour la connexion a mongoDB

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
+dotenv.config();
+const NAME = process.env.USER;
+const MDP = process.env.SECRET_PASS;
+const BDD = process.env.CLUSTER_NAME;
+
 const app = express();
 
 
-mongoose.connect('mongodb+srv://<name>>:<motdepasse>@<nomprojet>.j5cho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect("mongodb+srv://"+ NAME +":" + MDP +"@"+ BDD + ".j5cho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -24,7 +30,7 @@ app.use((req, res, next) => {
 
 
 app.use((req,res) => {
-    res.json({message : "Requête bien reçue !"});
+    res.json({message : "Requête bien reçue"});
 })
 
 module.exports = app;
