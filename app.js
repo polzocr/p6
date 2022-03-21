ce419274398d192b2afb2dd5eeb438b421c9019c
Polzocr <micro.cpt.62@outlook.fr>
2022-03-21 10:41:15 +0100

    configuration MongoDB + test .env

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+
+mongoose.connect('mongodb+srv://<name>>:<motdepasse>@<nomprojet>.j5cho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à nodemonMongoDB échouée !'));
 
 
 app.use(express.json()); //on recupère le corps JSON des requête POST
