bbf5bfaa8d14c35a36429aa70f99a348eada90d6
Polzocr <micro.cpt.62@outlook.fr>
2022-03-21 10:30:24 +0100

    configuration CORS et expres.json();

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+
+app.use(express.json()); //on recupère le corps JSON des requête POST
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+
 app.use((req,res) => {
     res.json({message : "Requête bien reçue !"});
 })
