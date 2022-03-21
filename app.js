826b2344a59516c916ed014d65f762c322cffef0
Polzocr <micro.cpt.62@outlook.fr>
2022-03-21 10:30:59 +0100

    la meme javais oublié de sauvegarder

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json()); //on recupère le corps JSON des requête POST
 
+//CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
